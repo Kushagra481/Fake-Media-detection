5dd15b97ffc6681183911be8005cb281d428871e
Adwait <adwait2021@gmail.com>
2025-02-23 23:26:47 +0530

    fix

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
+@app.route("/home")
 def index():
     return render_template("index.html", title="Home")
 
