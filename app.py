a34a58b4b8813be32eea9480ff683f3ebc0cbd4b
Kushagra Shukla <ks0092@srmist.edu.in>
2025-02-26 23:09:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def uploaded_file(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 if __name__ == "__main__":
-    app.run(debug=os.getenv("FLASK_DEBUG", "False") == "True")
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
